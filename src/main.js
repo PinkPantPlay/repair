639d2e77d9b71d142cb3120c26bf74806c5dcb54
PinkPantPlay <excel_0716@outlook.com>
2021-05-17 09:51:13 +0800

    commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App'
 
+//引入uview框架
+import uView from "uview-ui"
+Vue.use(uView)
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
@@ -8,4 +12,19 @@ App.mpType = 'app'
 const app = new Vue({
   ...App
 })
+
+//http请求
+import httpInterceptor from '@/services/http.interceptor.js'
+Vue.use(httpInterceptor, app)
+
+//API接口
+import proxy from '@/proxy'
+Vue.use(proxy, app)
+
+//引入自定义封装服务
+import auth from '@/services/auth.js'
+Vue.use(auth, app)
+
+
+
 app.$mount()
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,8 +1,22 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
+	<view class="home">
+		<view class="back">
+			<u-swiper height="1110" :list="list" mode="none"></u-swiper>
+		</view>
+
+		<view class="module">
+			<view class="box">
+				<view class="item">报修指南</view>
+				<view class="item">报修进度</view>
+				<view class="item">
+					<navigator url="/pages/product/list">
+						商品大全
+					</navigator>
+				</view>
+				<view class="item">
+					<button open-type="contact">在线客服</button>
+				</view>
+			</view>
 		</view>
 	</view>
 </template>
@@ -11,7 +25,19 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				list: [{
+						image: '/static/images/home/1.jpeg',
+						title: '昨夜星辰昨夜风，画楼西畔桂堂东'
+					},
+					{
+						image: '/static/images/home/2.jpeg',
+						title: '身无彩凤双飞翼，心有灵犀一点通'
+					},
+					{
+						image: '/static/images/home/3.jpeg',
+						title: '谁念西风独自凉，萧萧黄叶闭疏窗，沉思往事立残阳'
+					}
+				],
 			}
 		},
 		onLoad() {
@@ -23,27 +49,78 @@
 	}
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+<style lang="scss">
+	.home{
+		width:100%;
+		height:100%;
+		position: relative;
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
+		.back{
+			width:100%;
+			height:100%;
+		}
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+		.module{
+			position: absolute;
+			left:0;
+			top:0;
+			width:100%;
+			height:100%;
+			display:flex;
+			align-items:center;
+			text-align:center;
+
+
+			.box{
+				width:90%;
+				margin:0 auto;
+				display:flex;
+				align-items:center;
+				text-align:center;
+				flex-wrap:wrap;
+				justify-content: space-between;
+
+				.item{
+					width:49%;
+					height:7em;
+					line-height:7em;
+					text-align:center;
+					font-size:1.2em;
+					font-weight:bold;
+					margin-bottom:.5em;
+					color:#fff;
+
+					button{
+						background: transparent;
+						color: #fff;
+						width: 100%;
+						height: 100%;
+						line-height: 6.4em;
+						display: block;
+					}
+
+					//item:nth-child(1)
+					&:nth-child(1){
+						background:rgba(255,193,37,.5);
+					}
+
+					&:nth-child(2){
+						background:rgba(255,193,37,.5);
+					}
+
+					&:nth-child(3){
+						background:rgba(255,193,37,.5);
+					}
+
+					&:nth-child(4){
+						background:rgba(255,193,37,.5);
+					}
+				}
+				
+			}
+		}
+
+
 	}
 </style>
